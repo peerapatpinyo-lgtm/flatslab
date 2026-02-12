de72c0d069f9968d5e22265a26a5e32b20d99945
peerapatpinyo-lgtm <peerapat.pinyo@mail.kmutt.ac.th>
2026-02-12 11:39:44 +0700

    Update tab_ddm.py

--- tab_ddm.py
+++ tab_ddm.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt     
+import matplotlib.patches as patches
 from typing import Dict, Any, Tuple, Optional
 
 # ========================================================
